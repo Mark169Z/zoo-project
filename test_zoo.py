ad1f4cc01386d185d53bbeaa86a8dc2409ed60e8
Kasidit Trungjitvilas <90457798+Mark169Z@users.noreply.github.com>
2024-11-13 08:06:56 +0000

    Added test case / Fix zoo file

--- test_zoo.py
+++ test_zoo.py
@@ -5,10 +5,20 @@ class TestZoo(unittest.TestCase):
     def setUp(self):
         self.zoo = Zoo()
 
+    def test_invalid(self):
+        self.assertEqual(self.zoo.get_ticket_price(-1), None)
+
     def test_child_ticket_price(self):
-        self.assertEqual(self.zoo.get_ticket_price(5), 50)
+        self.assertEqual(self.zoo.get_ticket_price(8), 50)
        
-    # Add your additional test cases here.
+    def test_teenage_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(18), 100)
+        
+    def test_adult_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(40), 150)
+
+    def test_grans_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(70), 100)
 
 if __name__ == '__main__':
     unittest.main()
